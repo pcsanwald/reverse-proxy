651c832378dea4f79ee2401d6cabad5f5087f88a
paul <paul.sanwald@elastic.co>
2023-03-16 18:32:04 -0400

    implement blocking rules, read from config file, in the reverse proxy

--- listener.go
+++ listener.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -9,22 +10,32 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strings"
 )
 
-func main() {
-	body, err := os.ReadFile("proxy.config")
-	fmt.Println(string(body))
+// Define a struct to handle our configuration file format
 
-	// If config file isn't present, log but don't fail, the proxy can still
-	// provide some functionality.
-	if err != nil {
-		fmt.Printf("unable to read configuration file: %v", err)
+type Configuration struct {
+	Server string `json:"server"`
+	Rules  Deny   `json:"deny"`
+}
+type Deny struct {
+	Headers   []string `json:"headers"`
+	URLParams []string `json:"url-params"`
+}
+
+func main() {
+	configFileName := "config.json"
+	if len(os.Args) > 1 {
+		configFileName = os.Args[1]
 	}
+	body, err := os.ReadFile(configFileName)
+	if err != nil {
+		log.Fatal("Unable to load configuration file, %v. Please specify as an argument to the program.", err)
+	}
+	reverseProxyConfig := parseConfigFile(body)
 	fmt.Println("started reverse proxy...")
 
-	//TODO: remove hardcoded host, specify in config file
-	proxy, err := NewProxy("http://localhost:8080")
+	proxy, err := NewProxy(reverseProxyConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,18 +43,43 @@ func main() {
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
 
-// Implement RoundTrip function so that we can log the response to the request
-// from our reverse proxy.
-
-type loggingTransport struct {
+func parseConfigFile(configBytes []byte) *Configuration {
+	reverseProxyConfig := Configuration{}
+	err := json.Unmarshal(configBytes, &reverseProxyConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", reverseProxyConfig)
+	return &reverseProxyConfig
 }
 
+// Implement RoundTrip function so that we can log the response to the request
+// from our reverse proxy, as well as potentially block requests
+
+type loggingTransport struct {
+	config *Configuration
+}
+
+func shouldBlockRequest(request *http.Request, config *Configuration) bool {
+	for headerIndex := 0; headerIndex < len(config.Rules.Headers); headerIndex++ {
+		headerToBlock := config.Rules.Headers[headerIndex]
+		if request.Header.Get(headerToBlock) != "" {
+			return true
+		}
+	}
+
+	for queryParamIndex := 0; queryParamIndex < len(config.Rules.URLParams); queryParamIndex++ {
+		queryParamToBlock := config.Rules.URLParams[queryParamIndex]
+		if request.URL.Query().Has(queryParamToBlock) {
+			return true
+		}
+	}
+	return false
+}
 func (t *loggingTransport) RoundTrip(request *http.Request) (*http.Response, error) {
 	var response *http.Response
 	var err error
-	log.Printf("beginning %v", request)
-	if strings.Contains(request.URL.Path, "/foo") {
-		log.Println("Round trip: CONTAINS FOO RETURNING 403")
+	if shouldBlockRequest(request, t.config) {
 		response = &http.Response{
 			Status:        "403 Forbidden",
 			StatusCode:    403,
@@ -58,10 +94,10 @@ func (t *loggingTransport) RoundTrip(request *http.Request) (*http.Response, err
 	} else {
 		response, err = http.DefaultTransport.RoundTrip(request)
 	}
-	
+
 	// 3. The proxy should log all incoming requests, including headers and body, and response
 	// headers and body.
-	body, err := httputil.DumpResponse(response, true)
+	body, err := httputil.DumpResponse(response, response.Body == nil)
 	log.Printf("Response: %v", string(body))
 	if err != nil {
 		return nil, err
@@ -71,13 +107,13 @@ func (t *loggingTransport) RoundTrip(request *http.Request) (*http.Response, err
 
 // Creates a reverse proxy mapped to a targetHost
 
-func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
-	url, err := url.Parse(targetHost)
+func NewProxy(config *Configuration) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(config.Server)
 	if err != nil {
 		return nil, err
 	}
 	proxy := httputil.NewSingleHostReverseProxy(url)
-	proxy.Transport = &loggingTransport{}
+	proxy.Transport = &loggingTransport{config: config}
 	return proxy, nil
 }
 
@@ -85,15 +121,16 @@ func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
 // concerns separate from logging i/o
 
 func RequestAsLoggableString(request *http.Request) string {
-	logLine := fmt.Sprintf("Header: %s  Body: %s Method: %s URL: %s",
-		request.Header, request.Body, request.Method, request.URL)
-	return logLine
+	logLine, _ := httputil.DumpRequest(request, true)
+	return string(logLine)
 }
 
 // Use a custom handler so that we can log the request submitted to our reverse proxy
 
 func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		// 3. The proxy should log all incoming requests, including headers and body, and response
+		// headers and body.
 		log.Println(RequestAsLoggableString(request))
 		proxy.ServeHTTP(writer, request)
 
--- listener_test.go
+++ listener_test.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"regexp"
 	"testing"
 )
 
 func TestRequestAsLoggableString(t *testing.T) {
 	// TODO: this can be much more exhaustive
+	testURL, _ := url.Parse("/foo")
 	request := http.Request{
-		Method: "GET",
+		Method:           "GET",
+		URL:              testURL,
+		Proto:            "HTTP/1.1",
+		ProtoMajor:       1,
+		ProtoMinor:       1,
+		Header:           make(http.Header, 0),
+		Body:             nil,
+		GetBody:          nil,
+		ContentLength:    0,
+		TransferEncoding: nil,
+		Close:            false,
+		Host:             "localhost:9090",
+		Form:             nil,
+		PostForm:         nil,
+		MultipartForm:    nil,
+		Trailer:          nil,
+		RemoteAddr:       "",
+		RequestURI:       "",
+		TLS:              nil,
+		Response:         nil,
 	}
-	expectedOutputRegex := "Method: GET"
+	expectedOutputRegex := "GET /foo HTTP/1.1"
 	validLoggableString := regexp.MustCompile(expectedOutputRegex)
 	logString := RequestAsLoggableString(&request)
 	if !validLoggableString.MatchString(logString) {
